21eb311625c6f961e72f606ed73f966ec8b245b6
DravekD-Desktop <donovk3@rpi.edu>
2017-10-16 03:02:48 -0400

    Added some basic stuff.

---
+++ Game AI HW4/Assets/Scripts/Instantiate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate : MonoBehaviour {
+
+    Camera cam;
+    public Transform main_bird;
+    public Transform black_bird;
+    public Transform ob1;
+
+	// Use this for initialization
+	void Start () {
+        cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//cam.screenToWorldPoint(new Vector3(x, y, z));
+	}
+}
